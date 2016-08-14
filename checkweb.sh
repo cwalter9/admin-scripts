a641534c384e81dd9f5c18b4d3570f6ed74e4f17
Chad Walter <cwalter9@gmail.com>
2016-08-14 18:37:42 -0400

    Updates checkweb.sh to use tlog function. Added tlog function to functions

--- checkweb.sh
+++ checkweb.sh
@@ -2,6 +2,10 @@
 #
 # Simple script to check a website every 2 seconds. You can chance the sleep time to what you want in seconds. 
 #
+source admin.cfg
+. functions
+
+tlog `hostname -s` "Starting checkweb"
 
 function check() {
   curl http://website.com
